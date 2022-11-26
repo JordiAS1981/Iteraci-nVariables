88ac9435e9d8fa3bb7b158633696d7c8b2e0eecd
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-11-26 20:52:56 +0100

    Iteracion#3

---
+++ Iteracion#3.js
@@ -0,0 +1,28 @@
+const num1 = 10;
+const num2 = 5;
+const num3 = 2;
+const num4 = 15;
+const num5 = 9;
+const y = 10;
+const z = 5;
+
+const mult = num1 * num2; // ejercicio 1.1
+console.log("mult", mult);
+
+const divd = num1 / num3; // ejercicio 1.2
+console.log("divd", divd);
+
+const divi = num4 / num5; // ejercicio 1.3
+const modulo = num4 % num5;
+
+console.log ("divid", divi);
+console.log ("modulo", modulo);
+
+const xSuma = y + z; // ejercicio 1.4
+console.log ("xSuma", xSuma);
+
+const xMulti = y * z; // ejercicio 1.5
+console.log ("xMulti", xMulti);
+
+
+
