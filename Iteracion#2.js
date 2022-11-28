dbf2056c2debf12255ba5d41cd95160f8f37b4db
Jordi Peña Bootcamp <116197945+JordiAS1981@users.noreply.github.com>
2022-11-28 21:01:09 +0100

    Iteracion#5

---
+++ Iteracion#2.js
@@ -0,0 +1,18 @@
+// Ejercicio 1.1
+
+const character= "";
+const name = "Jack Sparrow";
+const age = 10;
+
+//-----------------
+
+// Ejercicio 1.2
+
+const firstName = "Jon";
+const lastName = "Snow";
+const ageJon = 24;
+console.log ("Soy " + firstName + " " + lastName + ", tengo " + ageJon + " años y me gustan los lobos.");
+
+// Ejercicio 1.3
+
+const Bu
